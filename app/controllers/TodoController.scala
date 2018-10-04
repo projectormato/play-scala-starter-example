49e500f3146afa7cb04d8e8b3e87e632d799ad64
projectormato <k.keita811.anuy@gmail.com>
2018-10-04 22:11:49 +0900

    add view todo list(no database) code for ToDo app.

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -6,9 +6,16 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 
+import services._
+
 class TodoController @Inject()(mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
   def helloworld() = Action{ implicit request: MessagesRequest[AnyContent] =>
     // ここのOKはステータスコード200で引数を返しますよってこと404ならNotFound()
     Ok("Hello World!")
+  }
+
+  def list() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val items: Seq[Todo] = Seq(Todo("Todo1"), Todo("Todo2"))
+    Ok(views.html.list(items))
   }
 }
